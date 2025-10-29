95528b50a6eb7ee90a9d553c02a217fdebbe31b2
Paul <98259426+Choooca@users.noreply.github.com>
2025-10-30 00:39:12 +0100

    [FIX] treesitters

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,16 +2,26 @@ return {
   "nvim-treesitter/nvim-treesitter",
   branch = 'master',
   lazy = false,
-  build = ":TSUpdate",
+  build = function()
+    -- Sur Windows, utilise une méthode alternative
+    if vim.fn.has("win32") == 1 then
+      require("nvim-treesitter.install").update({ with_sync = true })
+    else
+      vim.cmd("TSUpdate")
+    end
+  end,
   config = function()
-
     local config = require('nvim-treesitter.configs')
+
+    if vim.fn.has("win32") == 1 then
+      require('nvim-treesitter.install').compilers = { "clang", "gcc", "cl" }
+    end
+
     config.setup({
       auto_install = true,
-      ensure_installed = {"lua", "c", "cpp", "cmake", "make"},
-      highlight = { enable = true},
-      indent = { enable = true}
+      ensure_installed = { "lua", "c", "cpp", "cmake", "make" },
+      highlight = { enable = true },
+      indent = { enable = true }
     })
-
   end
 }
